ff6f1ca96e047e12425b61a393d982afd4566dde
nrcopeland <nrcopeland21@gmail.com>
2021-09-07 02:05:20 -0400

    basic calculator

--- src/App.js
+++ src/App.js
@@ -1,34 +1,79 @@
+import { useState } from "react";
+
 function App() {
-	return (
-		<div className="App">
-			<div className="Calculator">
-				<div className="Display">
-				<span>()0</span> 0
-				</div>
-			<div className="operator">
-				<button>/</button>
-				<button>*</button>
-				<button>+</button>
-				<button>-</button>
-				<button>DEL</button>
-				<button>.</button>
-				<button>=</button>
-			</div>
-			<div className="integers">
-				<button>0</button>
-				<button>1</button>
-				<button>2</button>
-				<button>3</button>
-				<button>4</button>
-				<button>5</button>
-				<button>6</button>
-				<button>7</button>
-				<button>8</button>
-				<button>9</button>
-			</div>
-			</div>
-		</div>
-	);
+  //easy way for creating a lot of buttons
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    if (
+      //cant start with op and cant have 2 op in a row
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      // eslint-disable-next-line no-eval
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const createIntegers = () => {
+    const digits = [];
+
+    for (let i = 0; i < 10; i++) {
+      digits.push(
+        <button key={i} onClick={() => updateCalc(i.toString())}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+
+  const calculate = () => {
+    // eslint-disable-next-line no-eval
+    setCalc(eval(calc).toString());
+  };
+
+  const deleteLast = () => {
+    if (calc === '') {
+      return;
+    }
+	const value = calc.slice(0,-1)
+
+    setCalc(value);
+  };
+
+  return (
+    <div className="App">
+      <div className="Calculator">
+        <div className="Display">
+          {/* if no calc value equals 0 */}
+          {result ? <span>({result})</span> : '' }&nbsp;
+          {calc || 0}
+        </div>
+        <div className="operator">
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={deleteLast}>DEL</button>
+          <button onClick={calculate}>=</button>
+        </div>
+        <div className="integers">
+          {createIntegers()}
+          <button onClick={calculate}>=</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
